8dfe34e896a4c3c5518e887f06960ffdd3186ab0
AshtonGrimmett <ashtongrimmett@icloud.com>
2026-06-09 16:22:07 +0100

    Recorded rating by time also

--- MainCode.R
+++ MainCode.R
@@ -1,14 +1,11 @@
-install.packages("sport")
 library(sport)
 library(tidyverse)
 library(PlayerRatings)
+library(ggplot2)
+library(dplyr)
 
-event_details <- read.csv("event_details.csv")
-fight_details <- read.csv("fight_details.csv")
-fighter_details <- read.csv("fighter_details.csv")
+
 UFC <- read.csv("UFC.csv")
-
-
 UFC <- UFC %>%
   mutate(date = as.Date(date),
          red_wins = case_when(
@@ -17,8 +14,10 @@ UFC <- UFC %>%
            TRUE ~ 0.5),
          period = as.integer(format(as.Date(date), "%Y%m")))
 
-train <- UFC %>% filter(date < as.Date("2023-01-01"))
-test  <- UFC %>% filter(date >= as.Date("2023-01-01"))
+
+train <- UFC %>% filter(date < as.Date("2025-01-01"))
+test  <- UFC %>% filter(date >= as.Date("2025-01-01"))
+
 
 ufc_glicko <- train %>%
   filter(!is.na(red_wins)) %>%
@@ -26,5 +25,47 @@ ufc_glicko <- train %>%
 
 
 result <- glicko2(ufc_glicko)
+results <- result$ratings
+
+head(results)
 
 
+
+
+
+
+periods <- sort(unique(ufc_glicko$period))
+
+ratings <- NULL
+history <- list()
+
+for (p in periods) {
+  
+  current_period <- filter(ufc_glicko, period == p)
+  
+  ratings <- glicko2(
+    current_period,
+    status = ratings
+  )$ratings
+  
+  history[[as.character(p)]] <-
+    ratings %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column("Fighter") %>%
+    mutate(period = p)
+}
+
+rating_history <- bind_rows(history)
+
+fighters <- c(
+  "Jon Jones",
+  "Islam Makhachev",
+  "Ilia Topuria"
+)
+
+rating_history %>%
+  filter(Player %in% fighters) %>%
+  ggplot(aes(period, Rating, colour = Player)) +
+  geom_line(linewidth = 1) +
+  geom_point() +
+  theme_minimal()
